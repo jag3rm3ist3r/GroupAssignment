f965958cd4dd843eb1257e06748cff1a90e0a32a
Etienne Bovet <ebovet@gmail.com>
2022-05-18 21:30:35 +1000

    Refactored recieving serial data to work with multiple nodes and moved into while loop

--- src/Edge.py
+++ src/Edge.py
@@ -6,13 +6,38 @@ import sys
 # MQTT for communication with the server.
 import paho.mqtt.publish as mqttpublish
 
-data = {"watered": 0,
+node1 = {"watered": 0,
         "button": 0,
         "water_level": 0,
         "light_level": 0,
         "time_record": 0,
         "last_update": 0}
 
+node2 = node1.copy()
+
+def sendData(topic, message):
+    targetip = sys.argv[2]
+    mqttpublish.single(topic, message, hostname=targetip)
+
+
+def serialDataFiltering(text):
+    #organize serial data as node dictionary
+    if (text[0].slice(0, 7) == "node1/"):
+        node = node1
+    if (text[0].slice(0, 7) == "node2/"):
+         node = node2
+
+    key = text[0].slice(7, -1)
+
+    if (
+            key == "watered" or
+            key == "button" or
+            key == "water_level" or
+            key == "light_level"
+        ):
+        #send the correct node's data
+        topic = f"{node}/{key}"
+        sendData(topic, text[1])
 
 def main():
     # Grab serial device (argument 1)
@@ -23,31 +48,11 @@ def main():
     # Read serial data into data dict. but only if there is data in the buffer.
     while(ser.in_waiting > 0):
         new_data = True
-        for i in range(0, 4):
+        #change from do 4 times to for every line in serial
+        for i in ser.in_waiting):
             text = ser.readline().decode('utf-8').strip().split(" = ")
-            #data coming in will have node name in front
-
-            if (
-                    text[0] == "watered" or
-                    text[0] == "button" or
-                    text[0] == "water_level" or
-                    text[0] == "light_level"
-                ):
-                data[text[0]] = text[1]
-    
-    # Do something with data if some was read.
-    if new_data:
-        seconds = time.time()
-        last_update = time.ctime(seconds)
-        data["time_record"] = seconds
-        data["last_update"] = time.ctime(seconds)
-
-        # Send data over MQTT.
-        topic = edge_name + "arduino"
-        message = "test"
-        targetip = sys.argv[2]
-        mqttpublish.single(topic, message, hostname=targetip)
-
+            #data coming in will have node name in front (ex: node1/)
+            serialDataFiltering(text)
 
     # !!! IMPLEMENT !!!
     # Receive MQTT signal to turn pump/LED on and off.
